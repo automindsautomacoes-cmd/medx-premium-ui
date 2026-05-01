7641d8b4a5b7cc9e8fd44b3b3589a20b47991534
automindsautomacoes-cmd <autominds.automacoes@gmail.com>
2026-04-30 21:17:37 -0300

    Deploy App routes and essential UI components.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Agenda from "./pages/Agenda";
 import FollowUp from "./pages/FollowUp";
+import Patients from "./pages/Patients";
 import NotFound from "./pages/NotFound";
 
 const App = () => (
@@ -13,6 +14,7 @@ const App = () => (
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/agenda" element={<Agenda />} />
       <Route path="/follow-up" element={<FollowUp />} />
+      <Route path="/patients" element={<Patients />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
